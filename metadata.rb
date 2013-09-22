d251bfebaabdc9ed6108ccd876ade562ca1809e2
Jesse Nelson <spheromak@gmail.com>
2013-09-22 09:56:36 -0700

    setup the connection in default recipe

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 name    "services"
-version "1.0.6"
+version "1.0.7"
 
 recommends "etcd"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,6 @@
-# this cook only loads these libs
+#  Services Cookbook
 #
+#  Recipe:: Default
+#  Setup the connection with the current run_context
+#
+Services::Connection.new run_context: run_context
