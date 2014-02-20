566259dd688b0768a8ba5eea42aa838412a424a4
Jesse Nelson <spheromak@gmail.com>
2014-02-20 13:38:13 -0800

    move to using jn_services gem

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,10 @@
 #  Recipe:: Default
 #  Setup the connection with the current run_context
 #
+
+# cookbok has been moved to a gem install it @ runtime
+chef_gem 'jn_services' do
+  action :nothing
+end.run_action(:install)
+
 Services::Connection.new run_context: run_context
--- test/spec/spec_helper.rb
+++
@@ -1,1 +0,0 @@
-require_relative "../../libraries/services.rb"
