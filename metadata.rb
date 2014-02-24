209e613ddc4ddde3958c8ff30031266ee23de067
Jesse Nelson <spheromak@gmail.com>
2014-02-23 18:27:03 -0800

    fix up to use the rubygems  release beta of etcd 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 name    'services'
-version '2.0.1'
+version '2.0.2'
 
 recommends 'etcd'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,20 +3,6 @@
 #  Recipe:: Default
 #  Setup the connection with the current run_context
 #
-# QQ this is what i gotta do for etcd for now
-gem_file = "#{Chef::Config[:cache_path]}/etcd-0.2.0.alpha.gem"
-
-if node[:ha_disabled]
-  directory Chef::Config[:cache_path] do
-    recursive true
-    action :nothing
-  end.run_action :create
-end
-
-remote_file gem_file do
-  action :nothing
-  source 'https://dl.dropboxusercontent.com/u/848501/etcd-0.2.0.alpha.gem'
-end.run_action :create
 
 chef_gem 'etcd' do
   action :install
@@ -24,7 +10,6 @@ chef_gem 'etcd' do
   options(force: true)
 end
 
-# cookbok has been moved to a gem install it @ runtime
 chef_gem 'jn_services' do
   version '1.0.5'
   action :install
